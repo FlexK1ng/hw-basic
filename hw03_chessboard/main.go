a9ceeb9f944dbb1d7d60103ffe31a35334f91a0e
Alexey Egorov <lifeisgoodvega@gmail.com>
2024-04-29 21:26:17 +0300

    hw03_chessboard Ready

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,37 @@
 package main
 
+import "fmt"
+
+func chessboard(size1 int, size2 int) {
+	for i := 0; i < size1; i++ {
+		chetstr := i%2 == 0
+		for j := 0; j < size2; j++ {
+			chetstlb := j%2 == 0
+			if chetstr {
+				if chetstlb {
+					fmt.Print("#")
+				} else {
+					fmt.Print(" ")
+				}
+			} else {
+				if chetstlb {
+					fmt.Print(" ")
+				} else {
+					fmt.Print("#")
+				}
+			}
+		}
+		fmt.Println()
+	}
+
+}
+
 func main() {
-	// Place your code here.
+
+	var size1, size2 int
+	n, err := fmt.Scanf("%d*%d", &size1, &size2)
+	if err != nil {
+		fmt.Println("Incorect", n, err)
+	}
+	chessboard(size1, size2)
 }
